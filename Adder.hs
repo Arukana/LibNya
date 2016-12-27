4054b1a805bc673fc32b5ee004c001d52f728ea2
adjivas <adjivas@users.noreply.github.com>
2016-12-27 05:36:37 +0100

    add data

--- Adder.hs
+++ Adder.hs
@@ -2,6 +2,46 @@
 module Adder where
 
 import Foreign.C
+import Foreign.C.Types
+
+data Attribute = NoneAttribute | Bold | Dim | Italic | Underline | Blink | Reverse | Hidden deriving (Enum)
+
+data Sheet = NoneSheet | Bust deriving (Enum)
+
+data Cardinal = UpperLeft | UpperMiddle | UpperRight | MiddleLeft | MiddleCentral | MiddleRight | LowerLeft | LowerMiddle | LowerRight deriving (Enum)
+
+data Part = NonePart | ArmLeft | ArmRight | Boobs | Clavicle | EarLeft | EarRight | EyeLeft | EyeRight | HairTop | HairLeft | HairRight | HandLeft | HandRight | Mouth | Tail | Bell | ExclamationMark | ExclamationMarks | Heart | Hearts | Lantern | QuestionMark | QuestionMarks | WoolBall
+
+data Emotion = NoneEmotion | Angry | Happy | Love | Malicious | Misunderstanding | Shocked | Sleepy | Speechless
+
+instance Enum Emotion where
+  fromEnum NoneEmotion = 95
+  fromEnum Angry = 97
+  fromEnum Happy = 104
+  fromEnum Love = 108
+  fromEnum Malicious = 109
+  fromEnum Misunderstanding = 105
+  fromEnum Shocked = 111
+  fromEnum Sleepy = 115
+  fromEnum Speechless = 101
+ 
+  toEnum 95 = NoneEmotion
+  toEnum 97 = Angry
+  toEnum 104 = Happy
+  toEnum 108 = Love
+  toEnum 109 = Malicious
+  toEnum 105 = Misunderstanding
+  toEnum 111 = Shocked
+  toEnum 115 = Sleepy
+  toEnum 101 = Speechless
+
+data Position = Position { cardinal :: Cardinal
+                         , cartesian :: (CUShort, CUShort)
+}
+
+data Tuple = Tuple { part :: Part
+                   , emotion :: Emotion
+}
 
 adder :: CInt -> CInt -> IO CInt
 adder x y = return $ x + y
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![Build Status](https://travis-ci.org/Arukana/libnya.svg?branch=haskell)](https://travis-ci.org/Arukana/libnya)
 
+ghc -o libadder.dylib -shared -static -fPIC Adder.hs StartEnd.c -lHSrts -lCffi
 gcc -shared -o libss.so -fPIC libss.c libadder.dylib -I"$(ghc --print-libdir)/include"
 install_name_tool -change "@rpath/libadder.dylib" "@executable_path/libadder.dylib" libss.so
 gcc -o main main.c
