a66d1d0e605295fe4e9f67524a6fe6210cdc32ba
adjivas <adjivas@users.noreply.github.com>
2017-01-06 08:21:54 +0100

    update submodule + translate rust example to c

--- src/lib.c
+++ src/lib.c
@@ -1,5 +1,9 @@
 #include "neko.h"
+#include <stdio.h>
 
 void start(t_lbstat *state, void ** _) {
 	state->sheet = Bust;
+	state->emotion[0][0].part = Heart;
+	state->emotion[0][0].emotion = Shocked;
+	state->position.cardinal = MiddleCentral;
 }
