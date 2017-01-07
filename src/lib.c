ba5be374cf55924a742614e3985fc5b656050be8
adjivas <adjivas@users.noreply.github.com>
2017-01-07 02:07:50 +0100

    pModule

--- src/lib.c
+++ src/lib.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 struct API {
-  void (*start)(t_lbstat *x, void **y);
-  void (*end)(t_lbstat *x, void **y);
-  char *(*dump_buffer)(char *buffer, int buffer_size);
-  int (*release_object)(char *obj);
+	void (*start)(t_lbstat *x, void **y);
+	void (*end)(t_lbstat *x, void **y);
+	char *(*dump_buffer)(char *buffer, int buffer_size);
+	int (*release_object)(char *obj);
 };
 
 void start(t_lbstat *lib, void **data);
@@ -15,21 +15,27 @@ void idle(t_lbstat *lib, void **data);
 struct API the_ffi;
 
 void start(t_lbstat *lib, void **data) {
-  PyObject *pName = NULL, *py_results = NULL;
-  PyObject *fill_api = NULL;
-  Py_Initialize();
-  PyRun_SimpleString("import sys;"
-                     "sys.path.insert(0, '.')");
-  Py_DECREF(pName);
-  py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
-  the_ffi.start(lib, (void **)data);
+	PyObject *pName = NULL, *pModule = NULL, *py_results = NULL;
+	PyObject *fill_api = NULL;
+	Py_Initialize();
+	PyRun_SimpleString("import sys;"
+					 "sys.path.insert(0, '.')");
+	pName = PyString_FromString("lib");
+	pModule = PyImport_Import(pName);
+	Py_DECREF(pName);
+	fill_api = PyObject_GetAttrString(pModule, "fill_api");
+	py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
+	the_ffi.start(lib, (void **)data);
 }
 
 void end(t_lbstat *lib, void **data) {
-  PyObject *pName = NULL, *py_results = NULL;
-  PyObject *fill_api = NULL;
-  Py_DECREF(pName);
-  py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
-  the_ffi.end(lib, (void **)data);
-  Py_Finalize();
+	PyObject *pName = NULL, *pModule = NULL, *py_results = NULL;
+	PyObject *fill_api = NULL;
+	pName = PyString_FromString("lib");
+	pModule = PyImport_Import(pName);
+	Py_DECREF(pName);
+	fill_api = PyObject_GetAttrString(pModule, "fill_api");
+	py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
+	the_ffi.end(lib, (void **)data);
+	Py_Finalize();
 }
