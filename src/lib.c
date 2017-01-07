0c2a05c537ec28a9fbcfe7c19c01b0afc06007b0
adjivas <adjivas@users.noreply.github.com>
2017-01-07 01:45:35 +0100

    clang-format

--- src/lib.c
+++ src/lib.c
@@ -3,41 +3,47 @@
 #include <stdio.h>
 
 struct API {
-	void (*start)(t_lbstat *x, void **y);
-	void (*end)(t_lbstat *x, void **y);
-	char* (*dump_buffer)(char *buffer, int buffer_size);
-	int (*release_object)(char *obj);
+  void (*start)(t_lbstat *x, void **y);
+  void (*end)(t_lbstat *x, void **y);
+  char *(*dump_buffer)(char *buffer, int buffer_size);
+  int (*release_object)(char *obj);
 };
 
 void start(t_lbstat *lib, void **data);
 void idle(t_lbstat *lib, void **data);
 
-#define PYVERIFY(exp) if ((exp) == 0) { fprintf(stderr, "%s[%d]: ", __FILE__, __LINE__); PyErr_Print(); exit(1); }
+#define PYVERIFY(exp)                                                          \
+  if ((exp) == 0) {                                                            \
+    fprintf(stderr, "%s[%d]: ", __FILE__, __LINE__);                           \
+    PyErr_Print();                                                             \
+    exit(1);                                                                   \
+  }
 
 struct API the_ffi;
 
-void start(t_lbstat *lib, void **data)
-{ PyObject *pName, *pModule, *py_results;
+void start(t_lbstat *lib, void **data) {
+  PyObject *pName, *pModule, *py_results;
   PyObject *fill_api;
   Py_Initialize();
-  PyRun_SimpleString
-  ( "import sys;"
-    "sys.path.insert(0, '.')" );
+  PyRun_SimpleString("import sys;"
+                     "sys.path.insert(0, '.')");
   PYVERIFY(pName = PyString_FromString("lib"))
   PYVERIFY(pModule = PyImport_Import(pName))
   Py_DECREF(pName);
   PYVERIFY(fill_api = PyObject_GetAttrString(pModule, "fill_api"))
   py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
   printf("  C_LIB::%lu\n", lib);
-  the_ffi.start(lib, (void**)data); }
+  the_ffi.start(lib, (void **)data);
+}
 
-void end(t_lbstat *lib, void **data)
-{ PyObject *pName, *pModule, *py_results;
+void end(t_lbstat *lib, void **data) {
+  PyObject *pName, *pModule, *py_results;
   PyObject *fill_api;
   PYVERIFY(pName = PyString_FromString("lib"))
   PYVERIFY(pModule = PyImport_Import(pName))
   Py_DECREF(pName);
   PYVERIFY(fill_api = PyObject_GetAttrString(pModule, "fill_api"))
   py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
-  the_ffi.end(lib, (void**)data);
-  Py_Finalize(); }
+  the_ffi.end(lib, (void **)data);
+  Py_Finalize();
+}
