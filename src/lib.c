0db707d40c65a9ed57042b54f91164933535770b
adjivas <adjivas@users.noreply.github.com>
2017-01-07 02:04:04 +0100

    remove macro and fix warning

--- src/lib.c
+++ src/lib.c
@@ -12,37 +12,23 @@ struct API {
 void start(t_lbstat *lib, void **data);
 void idle(t_lbstat *lib, void **data);
 
-#define PYVERIFY(exp)                                                          \
-  if ((exp) == 0) {                                                            \
-    fprintf(stderr, "%s[%d]: ", __FILE__, __LINE__);                           \
-    PyErr_Print();                                                             \
-    exit(1);                                                                   \
-  }
-
 struct API the_ffi;
 
 void start(t_lbstat *lib, void **data) {
-  PyObject *pName, *pModule, *py_results;
-  PyObject *fill_api;
+  PyObject *pName = NULL, *py_results = NULL;
+  PyObject *fill_api = NULL;
   Py_Initialize();
   PyRun_SimpleString("import sys;"
                      "sys.path.insert(0, '.')");
-  PYVERIFY(pName = PyString_FromString("lib"))
-  PYVERIFY(pModule = PyImport_Import(pName))
   Py_DECREF(pName);
-  PYVERIFY(fill_api = PyObject_GetAttrString(pModule, "fill_api"))
   py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
-  printf("  C_LIB::%lu\n", lib);
   the_ffi.start(lib, (void **)data);
 }
 
 void end(t_lbstat *lib, void **data) {
-  PyObject *pName, *pModule, *py_results;
-  PyObject *fill_api;
-  PYVERIFY(pName = PyString_FromString("lib"))
-  PYVERIFY(pModule = PyImport_Import(pName))
+  PyObject *pName = NULL, *py_results = NULL;
+  PyObject *fill_api = NULL;
   Py_DECREF(pName);
-  PYVERIFY(fill_api = PyObject_GetAttrString(pModule, "fill_api"))
   py_results = PyObject_CallFunction(fill_api, "k", &the_ffi);
   the_ffi.end(lib, (void **)data);
   Py_Finalize();
