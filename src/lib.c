b1bdb763a02e94bc6afb031d97d970651f06854a
adjivas <adjivas@users.noreply.github.com>
2017-01-29 16:42:31 +0100

    Bust to BustHappy

--- src/lib.c
+++ src/lib.c
@@ -16,7 +16,7 @@ void messagecpy(t_character *start, const unsigned char *source) {
 }
 
 void start(t_lbstat *lib, void ** _) {
-    lib->persona.sheet = Bust;
+    lib->persona.sheet = BustHappy;
     messagecpy(lib->tooltip.message, (const unsigned char *)"start\0");
 }
 
